dc85bcc187e8636e47ec82ea2d57e681689f1a7e
maxh76 <max.huenink76@gmail.com>
2014-07-15 17:29:34 -0500

    Create bukkitServer.sh

---
+++ bukkitServer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dir=/home/max/Desktop/bukkitServers/
+export dir
+#The DIRectory variable
+#Used to define the directory of bukkitServers, change as necessary.
+#Is exported to child 'serverBase.sh'
+
+bash /home/${USER}/Desktop/serverFiles/serverBase.sh
+#Starts 'serverBase.sh'
