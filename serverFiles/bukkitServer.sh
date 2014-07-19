3c60eecc7b7174bed5885023a9770543f7e07db0
maxh76 <max.huenink76@gmail.com>
2014-07-18 21:38:20 -0500

    Update bukkitServer.sh

--- serverFiles/bukkitServer.sh
+++ serverFiles/bukkitServer.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+sFiles="/home/${USER}/Desktop/serverFiles"
+#The directory for all server files
+
 dir=/home/max/Desktop/bukkitServers/
 export dir
 #The DIRectory variable
 #Used to define the directory of bukkitServers, change as necessary.
 #Is exported to child 'serverBase.sh'
 
-bash /home/${USER}/Desktop/serverFiles/serverBase.sh
+bash ${sFiles}/serverBase.sh
 #Starts 'serverBase.sh'
