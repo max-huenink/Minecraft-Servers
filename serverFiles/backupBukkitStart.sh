804d849fb64648e8303728615c3a5b9a0d7971e7
maxh76 <max.huenink76@gmail.com>
2014-07-18 21:35:55 -0500

    Update backupBukkitStart.sh

--- serverFiles/backupBukkitStart.sh
+++ serverFiles/backupBukkitStart.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+sFiles="/home/${USER}/Desktop/serverFiles"
+
 W=$(cat server.properties | grep level-name= | cut -c 12-40)
 #Gets level-name
 Z=$W"_$(date +%d-%m-%Y-%H:%M:%S).zip"
 #Defines what the zip filename should be
 zip -r ${Z} ${W}
 #Zips the world file (${W})recursively to ${Z}
-bash /home/${USER}/Desktop/serverFiles/regBukkitStart.sh
+bash ${sFiles}/regBukkitStart.sh
 #Starts the server
