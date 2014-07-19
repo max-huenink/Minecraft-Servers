2e16fefa26817920c3065321404235cc02e3c0fe
maxh76 <max.huenink76@gmail.com>
2014-07-18 21:39:20 -0500

    Update cleanStart.sh

--- serverFiles/cleanStart.sh
+++ serverFiles/cleanStart.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sFiles="/home/${USER}/Desktop/serverFiles"
+
 W=$(cat server.properties | grep level-name= | cut -c 12-40)
 #Gets level-name
 rm -r $W
@@ -8,5 +10,5 @@ unzip $W.zip
 #Unzips the world
 s=$(cat sType)
 #Gets the startFile to start the server with
-bash /home/max/Desktop/serverFiles/${s}
+bash ${sFiles}/${s}
 #Starts the proper server
