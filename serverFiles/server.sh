708ece608ee15a7ee4fac3b21108f4ba66ac2051
maxh76 <max.huenink76@gmail.com>
2014-07-18 21:43:26 -0500

    Update server.sh

--- serverFiles/server.sh
+++ serverFiles/server.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+sFiles="/home/${USER}/Desktop/serverFiles"
+#The directory for all server files
+
 dir=/home/${USER}/Desktop/minecraftServers/
 export dir
 #The DIRectory variable
 #Used to define the directory of minecraftServers, change as necessary.
 #Exported to child 'serverBase.sh'
 
-bash /home/${USER}/Desktop/serverFiles/serverBase.sh
+bash ${sFiles}/serverBase.sh
 #Starts 'serverBase.sh'
