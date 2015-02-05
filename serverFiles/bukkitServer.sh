e1671032ec8893e7717c29311e0b15a978a4337f
maxh76 <max.huenink76@gmail.com>
2015-02-04 20:44:23 -0600

    Update bukkitServer.sh

--- serverFiles/bukkitServer.sh
+++ serverFiles/bukkitServer.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-sFiles="/home/${USER}/Desktop/serverFiles"
-#The directory for all server files
+sFiles="null"
+dir="null"
+if [[ $OSTYPE = "linux-gnu" ]]; then
+        sFiles="/media/max/UNTITLED/servers/serverFiles"
+        dir="/media/max/UNTITLED/servers/bukkitServers"
+elif [[ $OSTYPE = "cygwin" ]]; then
+        sFiles="/cygdrive/e/servers/serverFiles"
+        dir="/cygdrive/e/servers/bukkitServers"
+fi
 
-dir=/home/max/Desktop/bukkitServers/
 export dir
 #The DIRectory variable
 #Used to define the directory of bukkitServers, change as necessary.
 #Is exported to child 'serverBase.sh'
+
+sType=bukkit
+export sType
 
 bash ${sFiles}/serverBase.sh
 #Starts 'serverBase.sh'
