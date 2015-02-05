8d74516352bb8eea0004369a75c8221089378303
maxh76 <max.huenink76@gmail.com>
2015-02-04 20:45:02 -0600

    Update cleanStart.sh

--- serverFiles/cleanStart.sh
+++ serverFiles/cleanStart.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
-
-sFiles="/home/${USER}/Desktop/serverFiles"
-
 W=$(cat server.properties | grep level-name= | cut -c 12-40)
 #Gets level-name
 rm -r $W
 #Removes world file and contents
-unzip $W.zip
+tar -xf $W.tar
 #Unzips the world
 s=$(cat sType)
 #Gets the startFile to start the server with
-bash ${sFiles}/${s}
+bash ${s}
 #Starts the proper server
