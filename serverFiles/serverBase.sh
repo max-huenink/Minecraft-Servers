224c63af5a66f65f3b6a9889c49e55815af13f15
maxh76 <max.huenink76@gmail.com>
2015-02-04 20:49:48 -0600

    Update serverBase.sh

--- serverFiles/serverBase.sh
+++ serverFiles/serverBase.sh
@@ -5,7 +5,6 @@ cd ${dir}
 ls -d --color=yes */
 #Lists all directories, with color.
 echo "Which server would you like to start?"
-sleep 0.1
 echo -n "Server: "
 read server
 
@@ -13,9 +12,25 @@ cd ${server}
 echo ""
 ls -1 --color=yes *sh
 #Lists each sh file, one per line
-echo "Which [type]Start.sh file would you like to use?"
-echo -n "Type: "
-read sh
+if [[ $sType = vanilla ]]; then
+        echo "Which [type]Start.sh file would you like to use?"
+        echo -n "Type: "
+        read sh
 
-bash ${sh}Start.sh
-#Starts the correct Start.sh file
+        bash ${sh}Start.sh
+        #Starts the correct Start.sh file
+elif [[ $sType = bukkit ]]; then
+        echo "Which [type]BukkitStart.sh file would you like to use?"
+        echo -n "Type: "
+        read sh
+
+        bash ${sh}BukkitStart.sh
+        #Starts the correct Start.sh file
+elif [[ $sType = forge ]]; then
+        echo "Which [type]ForgeStart.sh file would you like to use?"
+        echo -n "Type: "
+        read sh
+
+        bash ${sh}ForgeStart.sh
+        #Start the correct Start.sh file
+fi
