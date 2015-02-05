f7c34bfbaa68133d513dc4982ac6200248a7fc99
maxh76 <max.huenink76@gmail.com>
2015-02-04 20:49:28 -0600

    Update server.sh

--- serverFiles/server.sh
+++ serverFiles/server.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-sFiles="/home/${USER}/Desktop/serverFiles"
-#The directory for all server files
+sFiles="unknown"
+dir="unknown"
+if [[ $OSTYPE = "linux-gnu" ]]; then
+        sFiles="/media/max/UNTITLED/servers/serverFiles"
+        dir="/media/max/UNTITLED/servers/minecraftServers"
+elif [[ $OSTYPE = "cygwin" ]]; then
+        sFiles="/cygdrive/e/servers/serverFiles"
+        dir="/cygdrive/e/servers/minecraftServers"
+fi
 
-dir=/home/${USER}/Desktop/minecraftServers
 export dir
 #The DIRectory variable
 #Used to define the directory of minecraftServers, change as necessary.
 #Exported to child 'serverBase.sh'
+
+sType=vanilla
+export sType
 
 bash ${sFiles}/serverBase.sh
 #Starts 'serverBase.sh'
