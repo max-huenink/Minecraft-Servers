c73e2f4ae731b78c011a0c3c9bc4d625ec408b2c
maxh76 <max.huenink76@gmail.com>
2015-02-04 20:44:42 -0600

    Update cleanBukkitStart.sh

--- serverFiles/cleanBukkitStart.sh
+++ serverFiles/cleanBukkitStart.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-
-sFiles="/home/${USER}/Desktop/serverFiles"
-
 W=$(cat server.properties | grep level-name= | cut -c 12-40)
 #Gets level-name
 rm -r $W
 #Removes world file and contents
-unzip $W.zip
+tar -xf $W.tar
 #Unzips the world
-bash ${sFiles}/regBukkitStart.sh
-#Starts the server
+s=$(cat sType)
+#Gets the startFile to start the server with
+bash ${s}
+#Starts the proper server
