4ef3838abe6d26e808a43aa00d1479d598a5192a
maxh76 <max.huenink76@gmail.com>
2015-02-04 20:53:52 -0600

    Create newDownloadedForgeServer.sh

---
+++ serverFiles/newDownloadedForgeServer.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+sFiles="unknown"
+dir="unknown"
+if [[ $OSTYPE = "linux-gnu" ]]; then
+        sFiles="/media/max/UNTITLED/servers/serverFiles"
+        dir="/media/max/UNTITLED/servers/forgeServers"
+elif [[ $OSTYPE = "cygwin" ]]; then
+        sFiles="/cygdrive/e/servers/serverFiles"
+        dir="/cygdrive/e/servers/forgeServers"
+fi
+
+export sFiles
+#The directory for all server files
+#Is exported to child 'base.sh'
+
+export dir
+#The DIRectory variable
+#Used to define the directory of the minecraftServers, change as necessary.
+#Is exported to child 'base.sh'
+
+VERSION=forge
+export VERSION
+#The version variable
+#Used to define the current version
+#'reg' 'snap' or 'bukkit'
+#Is exported to child 'base.sh'
+
+DOWNLOAD=yes
+export DOWNLOAD
+#The download variable
+#Boolean: Tells if a world needs to be downloaded
+#'yes' or 'no'
+#Exported to child 'base.sh'
+
+bash ${sFiles}/base.sh
+#Starts 'base.sh'
